9e7a72e4a6929428ff1e1e8cc777c7f484279270
Sojeong Han <sohan@c8r3s14.42seoul.kr>
2021-06-24 17:26:10 +0900

    backup

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sohan <sohan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/18 17:53:09 by sohan             #+#    #+#             */
+/*   Updated: 2021/06/18 17:55:10 by sohan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "get_next_line.h"
@@ -10,17 +22,17 @@ int		main()
 	int		fd;
 	int		count = 0;
 
-	fd = open("5.txt", O_RDONLY);	
-	
-	//&line = NULL;	
+	fd = open("5.txt", O_RDONLY);
+
+	//&line = NULL;
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		printf("%s return : %d\n", line, ret);
+		printf("%s return3 : %d\n", line, ret);
 		free(line);
 		line = 0;
 		count++;
 	}
-	printf("%s return : %d\n", line, ret);
+	printf("%s return3 : %d\n", line, ret);
 	free(line);
 	line = 0;
 	close(fd);
